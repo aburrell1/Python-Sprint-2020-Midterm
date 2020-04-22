9e4da42d6f324b58bef6a14b0b0fc2043c9bbf3e
aburrell1 <46632750+aburrell1@users.noreply.github.com>
2020-04-22 13:04:46 -0400

    Last commit for today.
    1. Added the graph for recoveries(in orange).
    
    2. Create labels for all the graphs so that it is easier to tell the
    difference between each of them.

--- stats/covid_19_graph_us.py
+++ stats/covid_19_graph_us.py
@@ -20,46 +20,37 @@ with open(filename) as f:
     dates = []
     number_of_infections = []
     deaths = []
+    recoveries = []
 
     for row in reader:
         dates.append(row[0])
         number_of_infections.append(float(row[1]))
         deaths.append(float(row[2]))
-
-# data = [{
-#     'type': 'bar',
-#     'x': dates,
-#     'y': number_of_infections,
-#     'marker': {
-#         'color': 'rgb(255, 0, 0)',
-#         'line': {'width': 1.5, 'color': 'rgb(10, 10, 10)'},
-#     },
-#     'opacity': 0.5
-# }]
-#
-# my_layout = Layout(title='COVID-19 deaths in the US from March 1st, 2020 to April 20, 2020.')
-# my_layout = {
-#     'title': 'COVID-19 deaths in the US from March 1st, 2020 to April 20, 2020.',
-#     'xaxis': {'title': 'Dates'},
-#     'yaxis': {'title': 'Accumulative Number of Cases'},
-# }
-#
-# my_layout = Layout(title='World Fires')
-# fig = {'data': data, 'layout': my_layout}
-# offline.plot(fig, filename='world_fires_1_day.html')
+        recoveries.append(float(row[3]))
 
 fig, ax = plt.subplots()
-y_values = [element for element in number_of_infections]
 #ax.plot(dates, number_of_infections, c="red", alpha=0.5)
 
-ax.set_title("COVID-19 information(including deaths, recoveries, etc.) in the US from March 1st, 2020 to April 20, 2020.", fontsize=25)
+# Set title and labels.
+ax.set_title("COVID-19 information(including deaths, recoveries, etc.) in the US from March 1st, 2020 to April 20, 2020"
+             ".", fontsize=15)
 ax.set_xlabel("Dates", fontsize=5)
 fig.autofmt_xdate()
 ax.set_ylabel("Accumulative Number of Cases, Deaths, Recoveries, etc.", fontsize=5)
 ax.tick_params(axis="both", which="major", labelsize=6)
-print(number_of_infections)
+
+# Plot all rows
+plt.text(33, 750000, s='Number of Infections')
 plt.plot(dates, number_of_infections, '-o')
+
+plt.text(50, 1000, s='Deaths')
 plt.plot(dates, deaths, '-o', c='red')
+
+plt.text(50, 95000, s='Recoveries')
+plt.plot(dates, recoveries, '-o', c='orange')
+
+
+# Set axis
 ax.axis([-1, 52, -30000, 800000])
 
 # for i_x, i_y in zip(dates, number_of_infections):
