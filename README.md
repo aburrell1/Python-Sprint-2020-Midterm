d030a97966ff225cb8d5f72908bb13a4c6625998
aburrell1 <aburrell758@gmail.com>
2020-04-23 12:42:51 -0400

    Moved the information from the readme file to the top of the main program.

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
 # Python-Sprint-2020-Midterm
-
-# The information regarding infections, deaths, and recoveries opens when you click run.
-# The second graph contains the d_rates graphs. You have to close the first graph in order for this one to open.
-# When the second graph is closed, then the explanation of the d_rates will show up in the console.
--- stats/covid_19_graph_us.py
+++ stats/covid_19_graph_us.py
@@ -1,6 +1,12 @@
 import matplotlib.pyplot as plt
 import csv
 
+# READ THIS
+###################################################################################################################
+# The information regarding infections, deaths, and recoveries opens when you click run.
+# The second graph contains the d_rates graphs. You have to close the first graph in order for this one to open.
+# When the second graph is closed, then the explanation of the d_rates will show up in the console.
+###################################################################################################################
 
 filename = 'data/covid_19.csv'
 
