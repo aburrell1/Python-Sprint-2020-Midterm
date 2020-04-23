974e7cabc70b135704ab0e87595b072297d4db93
aburrell1 <46632750+aburrell1@users.noreply.github.com>
2020-04-23 12:35:00 -0400

    1. Updated the csv file to include d_rate1 and d_rate2. 2. Created a seperate graph for d_rate1 and d_rate2. 3. Created an explanation for d_rate1 and d_rate2 that will show up in    the console. 4. Added information about how to navigate through the graphs in    the readme. 5. Downloaded the json file containing the world data and added it to    the project.

--- stats/covid_19_graph_us.py
+++ stats/covid_19_graph_us.py
@@ -1,18 +1,10 @@
-#from plotly.graph_objs import Bar, Layout
-from plotly import offline
-import pandas
-import stats
-import plotly.express as px
-
 import matplotlib.pyplot as plt
 import csv
 
 
 filename = 'data/covid_19.csv'
 
-# Got the information on the markers for the line graph here:
-# https://stackoverflow.com/questions/8409095/matplotlib-set-markers-for-individual-points-on-a-line
-
+"""Process the csv file and extract information to use for the graph."""
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -21,16 +13,43 @@ with open(filename) as f:
     number_of_infections = []
     deaths = []
     recoveries = []
+    d_rates1 = []
+    d_rates2 = []
+
+    # Append the headers "d_rate1" and "d_rate2".
+    header_row.append("d_rate1")
+    header_row.append("d_rate2")
+
+    # Rewrite the file using the contents in this list. This is the best option I have right now. I do not know how
+    # to write to specific columns.
+    new_data = []
+
+    # In the rewritten file, write the header row first.
+    new_data.append(header_row)
 
     for row in reader:
+        # Append the data containing everything including the two d_rates.
+        new_data.append([row[0], row[1], row[2], row[3], float(row[2]) / float(row[1]),
+                         (float(row[2]) / float(row[3]))])
+
+        # Append the data from the file into the lists to use for the graphs.
         dates.append(row[0])
         number_of_infections.append(float(row[1]))
         deaths.append(float(row[2]))
         recoveries.append(float(row[3]))
+        d_rates1.append(float(row[2]) / float(row[1]))
+        d_rates2.append(float(row[2]) / float(row[3]))
+
+
+# Write the new data that includes the d_rates into the file.
+with open('data/covid_19.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerows(new_data)
+
+
+"""Set titles and labels"""
 
 fig, ax = plt.subplots()
-
-# Set title and labels.
 ax.set_title("COVID-19 information(including deaths, recoveries, etc.) in the US from March 1st, 2020 to April 20, 2020"
              ".", fontsize=15)
 ax.set_xlabel("Dates", fontsize=5)
@@ -38,18 +57,53 @@ fig.autofmt_xdate()
 ax.set_ylabel("Accumulative Number of Cases, Deaths, Recoveries, etc.", fontsize=5)
 ax.tick_params(axis="both", which="major", labelsize=6)
 
-# Plot all rows
+"""FIRST GRAPH"""
+# Infections
 plt.text(40, 750000, s='Number of Infections')
 plt.plot(dates, number_of_infections, '-o')
 
-plt.text(50, 1000, s='Deaths')
+# Deaths
+plt.text(51, 30000, s='Deaths')
 plt.plot(dates, deaths, '-o', c='red')
 
-plt.text(50, 95000, s='Recoveries')
+# Recoveries
+plt.text(51, 95000, s='Recoveries')
 plt.plot(dates, recoveries, '-o', c='orange')
 
-
-# Set axis
+# Set axis and plot the first graph
 ax.axis([-1, 52, -30000, 800000])
+plt.show()
+
+
+"""SECOND GRAPH(for the d_rates)"""
+fig, ax = plt.subplots()
+ax.set_title("COVID-19 death rates in the US from March 1st, 2020 to April 20, 2020"
+             ".", fontsize=15)
+ax.set_xlabel("Dates", fontsize=5)
+fig.autofmt_xdate()
+ax.set_ylabel("death rates at certain dates", fontsize=5)
+ax.tick_params(axis="both", which="major", labelsize=6)
+
+#Plot d_rates1
+plt.text(51, 0, s='d_rate1')
+plt.plot(dates, d_rates1, '-o', c='yellow')
+
+#Plot d_rates2
+plt.text(51, 0.6, s='d_rate2')
+plt.plot(dates, d_rates2, '-o', c='green')
 
 plt.show()
+
+"""Write d_rates information to the console"""
+
+print("The d_rates work like this: if the death rate grows higher than the infection or recovery rate, the d_rate "
+      "grows, otherwise it shrinks. \nIf the death rate and infection or recovery rates are similar, then the graph "
+      "remains stagnant.\n"
+      "d_rate1 starts out higher than d_rate2 because the number of infections starts higher than the number of "
+      "recoveries. \nTherefore the ratio of deaths/infections is lower than deaths/recoveries.\n"
+      "The number of infections starts out growing much faster than the number of recoveries.\n"
+      "Therefore d_rate1 remains low with a small ratio. Meanwhile, d_rate2 springs up because the number of "
+      "recoveries gets higher slowly whereas the death count goes up much faster.\n"
+      "However, the rate of recoveries starts to grow higher than the rate of deaths and therefore d_rate2 gets smaller"
+      ".\n"
+      "At the end, d_rate2 remains flat because the death rate and recovery rates are similar to each other.")
